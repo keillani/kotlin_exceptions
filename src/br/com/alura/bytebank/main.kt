0cc8b6e2104f4d31fd420833a3973bef695be3a7
keillani <keillani@github.com>
2021-10-26 23:09:25 -0300

    first commit

---
+++ src/br/com/alura/bytebank/main.kt
@@ -0,0 +1,59 @@
+package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelo.Endereco
+import java.lang.ClassCastException
+import java.lang.NumberFormatException
+
+fun main() {
+    println("início main")
+
+    //try-expression
+    //usar quando precisa pegar um valor de algum outra fonte
+    //exemplo abaixo uma string e preciso transformar em double
+    val entrada: String = "1"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException){
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null){
+        valorRecebido + 0.1
+    } else{
+        null
+    }
+
+    if(valorComTaxa!=null) {
+        println("Valor da taxa: $valorComTaxa")
+    } else {
+        println("Valor inválido")
+    }
+
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: ClassCastException){
+        e.printStackTrace()
+        println("ClassCastException foi pegada")
+    }
+
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
+}
