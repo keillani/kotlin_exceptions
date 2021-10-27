6e629cbc9f0153a9bd3c4fb317659dbc3c151ac4
keillani <keillani@github.com>
2021-10-26 23:11:03 -0300

    aula 02 tratando exceptions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -26,6 +26,15 @@ fun main() {
         null
     }
 
+//    val valorComTaxa: Double? = when {
+//        valorRecebido != null -> {
+//            valorRecebido + 0.1
+//        }
+//        else -> {
+//            null
+//        }
+//    }
+
     if(valorComTaxa!=null) {
         println("Valor da taxa: $valorComTaxa")
     } else {
